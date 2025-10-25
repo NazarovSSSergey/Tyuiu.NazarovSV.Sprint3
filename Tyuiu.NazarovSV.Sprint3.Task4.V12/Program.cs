494ad7a2c5856ff34e0322e22139ed8189a5f49f
Сергей Назаров <Tyuiu.NazarovSV@outlook.com>
2025-10-25 18:17:52 +0500

    Task4

---
+++ Tyuiu.NazarovSV.Sprint3.Task4.V12/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.NazarovSV.Sprint3.Task4.V12.Lib;
+namespace Tyuiu.NazarovSV.Sprint3.Task4.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+            
+            Console.WriteLine("Сумма ряда = " + ds.Calculate(startValue, stopValue));
+
+            Console.ReadKey();
+
+        }
+    }
+}
