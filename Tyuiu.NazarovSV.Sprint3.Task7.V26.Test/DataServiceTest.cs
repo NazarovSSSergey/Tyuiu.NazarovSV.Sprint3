31bc36f8a60556169a871f77d8891a239dca6e43
Сергей Назаров <Tyuiu.NazarovSV@outlook.com>
2025-10-26 13:57:52 +0500

    Task7

---
+++ Tyuiu.NazarovSV.Sprint3.Task7.V26.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.NazarovSV.Sprint3.Task7.V26.Lib;
+namespace Tyuiu.NazarovSV.Sprint3.Task7.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+
+            int len = stopValue - startValue + 1;
+            double[] valueNextArray;
+            valueNextArray = new double[len];
+            valueNextArray[0] = ;
+            valueNextArray[1] = ;
+            valueNextArray[2] = ;
+            valueNextArray[3] = ;
+            valueNextArray[4] = ;
+            valueNextArray[5] = ;
+            valueNextArray[6] = ;
+            valueNextArray[7] = ;
+            valueNextArray[8] = ;
+            valueNextArray[9] = ;
+            valueNextArray[10] = ;
+            double[] res;
+            res = new double[len];
+            res = ds.GetMassFunction(startValue, stopValue);
+            CollectionAssert.AreEqual(res, valueNextArray);
+        }
+    }
+}
---
+++ Tyuiu.NazarovSV.Sprint3.Task7.V26/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.NazarovSV.Sprint3.Task7.V26.Lib;
+namespace Tyuiu.NazarovSV.Sprint3.Task7.V26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+            double[] valueArray;
+            valueArray = new double[len];
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("+-----------+----------+");
+            Console.WriteLine("|    X      |   f(x)   |");
+            Console.WriteLine("+-----------+----------+");
+            for (int i = 0; i <= len-1; i++)
+            {
+                Console.WriteLine("|{0,5:d}      |  {1, 5:f2}   |", startValue, stopValue);
+                startValue++;
+            }
+            Console.WriteLine("+-----------+----------+");
+
+            Console.ReadKey();
+        }
+    }
+}
